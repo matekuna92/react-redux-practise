8cf54de4d4492fe6ce68a4d409e4b2f8001e587e
MATEKUNA <kuna.mate@parlament.hu>
2023-02-20 13:40:01 +0100

    useEffect fetch, reducers must be sideEffect free, cant use request inside them. UseEffect runs on app load, overwriting firebase data with empty array error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
-import { useSelector } from 'react-redux';
-
 function App() {
   const show = useSelector(state => state.ui.isCartVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+      fetch('https://react-redux-319a7-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+      });
+  }, [cart]);
 
   return (
     <Layout>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,37 +2,37 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const DUMMY_PRODUCTS = [
-  {
-    id: 'p1',
-    price: 6,
-    title: 'My First Book',
-    description: 'My First Book I"ve ever wrote'
-  },
-  {
-    id: 'p2',
-    price: 7,
-    title: 'My Second Book',
-    description: 'My Second Book I"ve ever wrote'
-  }
+    {
+        id: 'p1',
+        price: 6,
+        title: 'My First Book',
+        description: 'My First Book I"ve ever wrote'
+    },
+    {
+        id: 'p2',
+        price: 7,
+        title: 'My Second Book',
+        description: 'My Second Book I"ve ever wrote'
+    }
 ];
 
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-				{DUMMY_PRODUCTS.map(product => (
-					<ProductItem
-						key={product.id}
-            id={product.id}
-						title={product.title}
-						price={product.price}
-						description={product.description}
-					/>)
-			)}
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(product => (
+                    <ProductItem
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        price={product.price}
+                        description={product.description}
+                    />)
+                )}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
